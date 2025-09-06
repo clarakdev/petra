d2e47922b1fe2741dc6e8b15a7afe85927ab34c7
moonbby <88180557+moonbby@users.noreply.github.com>
2025-09-07 00:20:50 +1200

    feat: add clickable pet selection with scaling, PetSelector container, and UI Image sprites
    - Pets are now clickable; selected pet smoothly scales up
    - Only one pet remains enlarged at a time
    - PetSelector set as container for option prefabs
    - Converted pet display from SpriteRenderer to UI Image for proper UI integration

---
+++ Assets/Scripts/Pet.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+[Serializable]
+public class Pet
+{
+    public GameObject prefab;
+    public string name;
+    public Sprite icon;
+    
+}
---
+++ Assets/Scripts/PetSelectionManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PetSelectionManager : MonoBehaviour
+{
+    public static PetSelectionManager instance;
+    public Pet[] pets;
+    public Pet currentPet;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        if (pets.Length > 0 && currentPet == null)
+        {
+            currentPet = pets[0];
+        }
+    }
+
+    public void SetPet(Pet pet)
+    {
+        currentPet = pet;
+        Debug.Log("[PetSelectionManager] Current pet = " + pet.name);
+    }
+}
