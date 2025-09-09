9db9f2d9076f6a4b7e2c0d6e6dc2b7239cacc91c
shanamuse <shanamusekiwa@gmail.com>
2025-09-10 09:41:55 +1200

    update feat:purchaser, new scene

--- Assets/Scripts/ShopUI_Item.cs
+++ Assets/Scripts/ShopUI_Item.cs
@@ -1,22 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 using TMPro;
 
 public class ShopUI_Item : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI ItemName;
-    [SerializeField] private TextMeshProUGUI Description;
-    [SerializeField] private TextMeshProUGUI Price;
+    [SerializeField] TextMeshProUGUI ItemName;
+    [SerializeField] TextMeshProUGUI Description;
+    [SerializeField] TextMeshProUGUI Price;
+    [SerializeField] Image BackgroundPanel;
+    [SerializeField] Color DefaultColour = Color.white;
+    [SerializeField] Color SelectedColour = Color.yellow;
 
-    private ShopItem Item;
+    ShopItem Item;
+    UnityAction<ShopItem> OnSelectedFn;
 
     public void Bind(ShopItem item)
     {
+        Bind(item, null);
+    }
+
+    public void Bind(ShopItem item, UnityAction<ShopItem> onSelectedFn)
+    {
         Item = item;
+        OnSelectedFn = onSelectedFn;
 
         ItemName.text = Item.Name;
         Description.text = Item.Description;
-        Price.text = $"${(Item.Cost / 100):0.00}";
+        Price.text = $"${(Item.Cost / 100f):0.00}";
+
+        SetIsSelected(false);
+    }
+
+    public void SetIsSelected(bool selected)
+    {
+        if (BackgroundPanel != null)
+            BackgroundPanel.color = selected ? SelectedColour : DefaultColour;
+    }
+
+    public void SetCanAfford(bool canAfford)
+    {
+        if (Price != null)
+        {
+            Price.fontStyle = canAfford ? FontStyles.Normal : FontStyles.Strikethrough;
+            Price.color = canAfford ? Color.white : Color.red;
+        }
+    }
+
+    public void OnClicked()
+    {
+        OnSelectedFn?.Invoke(Item);
     }
 }
