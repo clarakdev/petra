5a270df2eae7e2203c72fefef21f537b94241d03
shanamuse <shanamusekiwa@gmail.com>
2025-09-19 01:55:15 +1200

    shop icons done/ shop completed

--- Assets/Scripts/ShopItem.cs
+++ Assets/Scripts/ShopItem.cs
@@ -1,12 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Shop/Item", fileName = "ShopItem_")]
+[CreateAssetMenu(fileName = "ShopItem_", menuName = "Shop/Item")]
 public class ShopItem : ScriptableObject
 {
     public ShopItemCategory Category;
     public string Name;
     [TextArea(3, 5)] public string Description;
     public int Cost;
+    public Sprite Icon;
 }
