f229ca614ef2d18a6d9d862b64234cd10b16e2c1
moonbby <88180557+moonbby@users.noreply.github.com>
2025-10-17 15:44:30 +1300

    feat: connect PetBattle to HealthBar and add TakeDamage logic

--- Assets/Scripts/PetBattle.cs
+++ Assets/Scripts/PetBattle.cs
@@ -6,12 +6,25 @@ public class PetBattle : MonoBehaviourPun
     public Sprite battleSpritePlayerSide; // Player's pet, seen from behind
     public Sprite battleSpriteEnemySide;  // Opponent's pet, seen from the front
 
-    public int maxHealth;
+    public int maxHealth = 100;
     public int currentHealth;
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
 
     public void SetFacing(bool isPlayerSide)
     {
         var spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = isPlayerSide ? battleSpritePlayerSide : battleSpriteEnemySide;
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
     }
 }
