4d9e3cea68bb5e96ebb221da110efb54409f1a30
moonbby <88180557+moonbby@users.noreply.github.com>
2025-10-17 16:20:27 +1300

    feat: add playerStatusPanel reference to BattleUIManager

--- Assets/Scripts/BattleUIManager.cs
+++ Assets/Scripts/BattleUIManager.cs
@@ -4,18 +4,21 @@ public class BattleUIManager : MonoBehaviour
 {
     [SerializeField] private GameObject commandPanel;
     [SerializeField] private GameObject movePanel;
+    [SerializeField] private GameObject playerStatusPanel;
 
     private void Start() => ShowCommandMenu();
 
     public void ShowCommandMenu()
     {
         commandPanel.SetActive(true);
+        playerStatusPanel.SetActive(true);
         movePanel.SetActive(false);
     }
 
     public void OnFightClicked()
     {
         commandPanel.SetActive(false);
+        playerStatusPanel.SetActive(false);
         movePanel.SetActive(true);
     }
 }
