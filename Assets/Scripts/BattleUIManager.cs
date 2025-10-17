cd596a1fd4f3c587504a39e0a2b02d02cb7329a2
moonbby <88180557+moonbby@users.noreply.github.com>
2025-10-17 15:09:14 +1300

    feat: add player status UI and simple HealthBar script

--- Assets/Scripts/BattleUIManager.cs
+++ Assets/Scripts/BattleUIManager.cs
@@ -1,51 +1,21 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class BattleUIManager : MonoBehaviour
 {
-    [SerializeField] private GameObject commandPanel; // BattleUI/CommandPanel
-    [SerializeField] private GameObject movePanel;    // BattleUI/MovePanel
-    [SerializeField] private Button fightButton;      // BattleUI/CommandPanel/FightButton
-    [SerializeField] private Button backButton;       // BattleUI/MovePanel/BackButton (optional)
-
-    private void Awake()
-    {
-        // Auto-wire by name if not set in Inspector.
-        if (!commandPanel) commandPanel = transform.Find("CommandPanel")?.gameObject;
-        if (!movePanel) movePanel = transform.Find("MovePanel")?.gameObject;
-
-        if (!fightButton && commandPanel)
-            fightButton = commandPanel.transform.Find("FightButton")?.GetComponent<Button>();
-
-        if (!backButton && movePanel)
-            backButton = movePanel.transform.Find("BackButton")?.GetComponent<Button>();
-    }
-
-    private void OnEnable()
-    {
-        if (fightButton) fightButton.onClick.AddListener(OnFightClicked);
-        if (backButton) backButton.onClick.AddListener(OnBackClicked);
-    }
-
-    private void OnDisable()
-    {
-        if (fightButton) fightButton.onClick.RemoveListener(OnFightClicked);
-        if (backButton) backButton.onClick.RemoveListener(OnBackClicked);
-    }
+    [SerializeField] private GameObject commandPanel;
+    [SerializeField] private GameObject movePanel;
 
     private void Start() => ShowCommandMenu();
 
     public void ShowCommandMenu()
     {
-        if (commandPanel) commandPanel.SetActive(true);
-        if (movePanel) movePanel.SetActive(false);
+        commandPanel.SetActive(true);
+        movePanel.SetActive(false);
     }
 
     public void OnFightClicked()
     {
-        if (commandPanel) commandPanel.SetActive(false);
-        if (movePanel) movePanel.SetActive(true);
+        commandPanel.SetActive(false);
+        movePanel.SetActive(true);
     }
-
-    public void OnBackClicked() => ShowCommandMenu();
 }
