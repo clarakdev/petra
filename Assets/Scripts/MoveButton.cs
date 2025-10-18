8bc7cc96eebdd8a43df333fa851aa300cf27e4a2
moonbby <88180557+moonbby@users.noreply.github.com>
2025-10-18 16:30:59 +1300

    fix: return to command panel after move selection in MoveButton

--- Assets/Scripts/MoveButton.cs
+++ Assets/Scripts/MoveButton.cs
@@ -6,14 +6,20 @@ public class MoveButton : MonoBehaviour
     public DamageData move;                 // set per button in Inspector
     public BattleManager manager;
     [SerializeField] private TMP_Text label;
+    [SerializeField] private BattleUIManager ui;
 
     private void Awake()
     {
+        if (!ui) ui = GetComponentInParent<BattleUIManager>();
         if (label && move) label.text = move.moveName.ToUpper();
     }
 
     public void OnClick()
     {
-        manager.PlayerAttack(move.power);
+        if (manager && move)
+            manager.PlayerAttack(move.power);
+
+        // Return to the command panel immediately after choosing a move
+        if (ui) ui.ShowCommandMenu();
     }
 }
