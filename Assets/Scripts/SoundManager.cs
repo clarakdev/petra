5fd134db87e6e0e881406170e72dd3d48f35f3b0
yarm03 <jwoconnor03@gmail.com>
2025-10-27 13:50:51 +1300

    New sound scripts

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,30 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
-    [SerializeField] Slider volumeSlider;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static SoundManager Instance;
+
+    [Header("UI (Optional in this scene)")]
+    [SerializeField] private Slider volumeSlider;
+
+    [Header("Music Player")]
+    [SerializeField] private AudioSource musicSource;   // <-- you'll assign this in Inspector
+
+    private void Awake()
     {
-        if(!PlayerPrefs.HasKey("musicVolume")) {
-            PlayerPrefs.SetFloat("musicVolume", 1);
-            Load();
+        // make this persist (optional but recommended if you want music across scenes)
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        // init saved volume
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1f);
+        }
+
+        float savedVol = PlayerPrefs.GetFloat("musicVolume");
+        AudioListener.volume = savedVol;
+
+        if (volumeSlider != null)
+        {
+            volumeSlider.value = savedVol;
+            volumeSlider.onValueChanged.AddListener(v => ChangeVolume(v));
+        }
+
+        // start music if not already playing
+        if (musicSource != null && !musicSource.isPlaying)
+        {
+            musicSource.loop = true;       // safety in case you forget in Inspector
+            musicSource.Play();
         }
     }
 
-    public void ChangeVolume() {
-        AudioListener.volume = volumeSlider.value;
-        Save();
+    // this gets called by the slider listener
+    public void ChangeVolume(float newVol)
+    {
+        AudioListener.volume = newVol;
+        PlayerPrefs.SetFloat("musicVolume", newVol);
     }
 
-    private void Load() {
-        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
-    }
+    // if you open an options menu scene later and need to hook a new slider:
+    public void RegisterSlider(Slider newSlider)
+    {
+        volumeSlider = newSlider;
 
-    private void Save() {
-        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+        float currentVol = PlayerPrefs.GetFloat("musicVolume");
+        volumeSlider.value = currentVol;
+
+        volumeSlider.onValueChanged.AddListener(v => ChangeVolume(v));
     }
 }
